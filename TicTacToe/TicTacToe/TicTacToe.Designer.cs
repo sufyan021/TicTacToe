13afbeee9f7585f70df9a9600738cf7a7ba9d72a
sufyan021 <sufyan015@outlook.com>
2023-01-11 16:14:06 +0000

    simple tictactoe game that I've made

---
+++ TicTacToe/TicTacToe/TicTacToe.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace TicTacToe
+{
+    partial class TicTacToe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(320, 381);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 73);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(282, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 34);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Tic Tac Toe Game";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Location = new System.Drawing.Point(260, 85);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(65, 60);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Location = new System.Drawing.Point(366, 84);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(70, 65);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox3.Location = new System.Drawing.Point(482, 85);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(65, 60);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox4.Location = new System.Drawing.Point(259, 175);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(65, 60);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 8;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox5.Location = new System.Drawing.Point(259, 258);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(65, 60);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 9;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox6.Location = new System.Drawing.Point(482, 175);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(65, 60);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 10;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox7.Location = new System.Drawing.Point(482, 258);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(65, 60);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 11;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox8.Location = new System.Drawing.Point(366, 172);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(70, 65);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 12;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Click += new System.EventHandler(this.pictureBox8_Click);
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox9.Location = new System.Drawing.Point(365, 261);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(70, 65);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox9.TabIndex = 13;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.Click += new System.EventHandler(this.pictureBox9_Click);
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Image = global::TicTacToe.Properties.Resources.X;
+            this.pictureBox10.Location = new System.Drawing.Point(733, 84);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(105, 102);
+            this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox10.TabIndex = 14;
+            this.pictureBox10.TabStop = false;
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.Image = global::TicTacToe.Properties.Resources.O;
+            this.pictureBox11.Location = new System.Drawing.Point(733, 215);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(105, 103);
+            this.pictureBox11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox11.TabIndex = 15;
+            this.pictureBox11.TabStop = false;
+            this.pictureBox11.Click += new System.EventHandler(this.pictureBox11_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(606, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 34);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Player 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(606, 243);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 34);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Player 2";
+            // 
+            // TicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(940, 492);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "TicTacToe";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ TicTacToe/TicTacToe/TicTacToe.cs
@@ -0,0 +1,393 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using TicTacToe.Properties;
+
+namespace TicTacToe
+{
+    public partial class TicTacToe : Form
+    {
+        public TicTacToe()
+        {
+            InitializeComponent();
+        }
+
+        private Boolean P1;
+
+        private Boolean P2;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+            System.Drawing.Graphics MyGraphics;
+
+            MyGraphics = CreateGraphics();
+
+            Pen Colour = new Pen(System.Drawing.Color.Black);
+
+            MyGraphics.DrawLine(Colour, 250, 65, 250, 265);
+
+            MyGraphics.DrawLine(Colour, 350, 65, 350, 265);
+
+            MyGraphics.DrawLine(Colour, 185, 125, 415, 125);
+
+            MyGraphics.DrawLine(Colour, 185, 200, 415, 200);
+
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+
+            pictureBox1.Image = null;
+
+            pictureBox2.Image = null;
+
+            pictureBox3.Image = null;
+
+            pictureBox4.Image = null;
+
+            pictureBox5.Image = null;
+
+            pictureBox6.Image = null;
+
+            pictureBox7.Image = null;
+
+            pictureBox8.Image = null;
+
+            pictureBox9.Image = null;
+
+            button1.Text = "Restart";
+
+            P1 = true;
+
+            P2 = false;
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+            if (pictureBox1.Image is null)
+
+            {
+
+                if (P1 == true)
+
+                {
+
+                    pictureBox1.Image = global::TicTacToe.Properties.Resources.X;
+
+                    P1 = false;
+
+                    P2 = true;
+
+                }
+
+                else if (P2 == true)
+
+                {
+
+                    pictureBox1.Image = global::TicTacToe.Properties.Resources.O;
+
+                    P2 = false;
+
+                    P1 = true;
+
+                }
+
+            }
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+            if (pictureBox2.Image is null)
+
+            {
+
+                if (P1 == true)
+
+                {
+
+                    pictureBox2.Image = global::TicTacToe.Properties.Resources.X;
+
+                    P1 = false;
+
+                    P2 = true;
+
+                }
+
+                else if (P2 == true)
+
+                {
+
+                    pictureBox2.Image = global::TicTacToe.Properties.Resources.O;
+
+                    P2 = false;
+
+                    P1 = true;
+
+                }
+
+            }
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+            if (pictureBox3.Image is null)
+
+            {
+
+                if (P1 == true)
+
+                {
+
+                    pictureBox3.Image = global::TicTacToe.Properties.Resources.X;
+
+                    P1 = false;
+
+                    P2 = true;
+
+                }
+
+                else if (P2 == true)
+
+                {
+
+                    pictureBox3.Image = global::TicTacToe.Properties.Resources.O;
+
+                    P2 = false;
+
+                    P1 = true;
+
+                }
+
+            }
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+            if (pictureBox4.Image is null)
+
+            {
+
+                if (P1 == true)
+
+                {
+
+                    pictureBox4.Image = global::TicTacToe.Properties.Resources.X;
+
+                    P1 = false;
+
+                    P2 = true;
+
+                }
+
+                else if (P2 == true)
+
+                {
+
+                    pictureBox4.Image = global::TicTacToe.Properties.Resources.O;
+
+                    P2 = false;
+
+                    P1 = true;
+
+                }
+
+            }
+
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+
+            if (pictureBox8.Image is null)
+
+            {
+
+                if (P1 == true)
+
+                {
+
+                    pictureBox8.Image = global::TicTacToe.Properties.Resources.X;
+
+                    P1 = false;
+
+                    P2 = true;
+
+                }
+
+                else if (P2 == true)
+
+                {
+
+                    pictureBox8.Image = global::TicTacToe.Properties.Resources.O;
+
+                    P2 = false;
+
+                    P1 = true;
+
+                }
+
+            }
+
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+
+            if (pictureBox6.Image is null)
+
+            {
+
+                if (P1 == true)
+
+                {
+
+                    pictureBox6.Image = global::TicTacToe.Properties.Resources.X;
+
+                    P1 = false;
+
+                    P2 = true;
+
+                }
+
+                else if (P2 == true)
+
+                {
+
+                    pictureBox6.Image = global::TicTacToe.Properties.Resources.O;
+
+                    P2 = false;
+
+                    P1 = true;
+
+                }
+
+            }
+
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+            if (pictureBox5.Image is null)
+
+            {
+
+                if (P1 == true)
+
+                {
+
+                    pictureBox5.Image = global::TicTacToe.Properties.Resources.X;
+
+                    P1 = false;
+
+                    P2 = true;
+
+                }
+
+                else if (P2 == true)
+
+                {
+
+                    pictureBox5.Image = global::TicTacToe.Properties.Resources.O;
+
+                    P2 = false;
+
+                    P1 = true;
+
+                }
+
+            }
+
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+
+            if (pictureBox9.Image is null)
+
+            {
+
+                if (P1 == true)
+
+                {
+
+                    pictureBox9.Image = global::TicTacToe.Properties.Resources.X;
+
+                    P1 = false;
+
+                    P2 = true;
+
+                }
+
+                else if (P2 == true)
+
+                {
+
+                    pictureBox9.Image = global::TicTacToe.Properties.Resources.O;
+
+                    P2 = false;
+
+                    P1 = true;
+
+                }
+
+            }
+
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+
+            if (pictureBox7.Image is null)
+
+            {
+
+                if (P1 == true)
+
+                {
+
+                    pictureBox7.Image = global::TicTacToe.Properties.Resources.X;
+
+                    P1 = false;
+
+                    P2 = true;
+
+                }
+
+                else if (P2 == true)
+
+                {
+
+                    pictureBox7.Image = global::TicTacToe.Properties.Resources.O;
+
+                    P2 = false;
+
+                    P1 = true;
+
+                }
+
+            }
+
+        }
+
+        private void pictureBox11_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
